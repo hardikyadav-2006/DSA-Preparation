8477f49a1de74168119487ce52be89fd73cc9958
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-18 20:21:11 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = -1;
+        for(int i = 0; i<n; i++){
+            if(nums[i] != 0){
+                swap(nums[j+1], nums[i]);
+                j++;
+            }
+        }
+        
+    }
+};
