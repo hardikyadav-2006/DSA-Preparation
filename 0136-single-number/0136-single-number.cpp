e004d5bfec3691903c10e50ea7adaa2b625461bf
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-18 21:16:13 +0530

    Time: 0 ms (100%), Space: 20.6 MB (82.48%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int i =0;
+        int res;
+        while(i<n){
+            if(i+1 == n){
+                res = nums[i];
+            }
+            else if(nums[i]!=nums[i+1] || i+1 == n){
+                res= nums[i];
+                break;
+            }
+            i=i+2;
+        }
+        return res;
+    }
+};
