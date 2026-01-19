218eeeb8466c081e0b1b341e96c0fa831442f012
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-19 19:53:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
+        int max =0;
         unordered_map<int,int> m;
         for(int i = 0; i<n; i++){
             if(m.find(nums[i]) == m.end()){
@@ -9,13 +10,10 @@ public:
             }else{
                 m[nums[i]] += 1;
             }
-        }
-        int max = 0;
-        for(int i = 0; i<n; i++){
+            
             if(m[nums[i]] > n/2){
-                max= nums[i];
-                break;
-            }
+                    max= nums[i];
+                    break;
         }
         return max;
         
