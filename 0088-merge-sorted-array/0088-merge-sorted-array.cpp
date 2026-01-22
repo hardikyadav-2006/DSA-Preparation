71ef55b30df26bac4c66f726a17e9e10c45c7667
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-22 09:10:00 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,29 +4,23 @@ public:
         int i = m+n-1;
         int j = m-1;
         int k = n-1;
-        if(n == 0){
-            return;
-        }
-        while(k>=0 && i>= 0){
-            if(j == -1){
-                nums1[i] =nums2[k];
-                i--;
-                k--;
-            }
 
-            else if(nums1[j] <= nums2[k]){
+        while(i>=0 && k>=0){
+            if(j==-1){
                 nums1[i] = nums2[k];
                 i--;
                 k--;
-            }else if(nums1[j] > nums2[k]){
-                swap(nums1[j], nums1[i]);
-                nums1[j] = nums2[k];
-                j--;
+            }
+            else if(nums1[j]<=nums2[k]){
+                nums1[i] = nums2[k];
                 i--;
                 k--;
+            }else{
+                swap(nums1[i], nums1[j]);
+                j--;
+                i--;
             }
         }
-
         
     }
 };
