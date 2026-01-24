ca15b3eb1111da4146ee96d2051195006e4ce48e
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-24 13:31:18 +0530

    Time: 2 ms (92.69%), Space: 23.9 MB (32.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,9 +3,11 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         vector<vector<int>> res;
+        
         sort(intervals.begin(), intervals.end());
-        for(int i = 0; i<n; i++){
-            if(res.empty() || intervals[i][0] > res.back()[1]){
+        res.push_back(intervals[0]);
+        for(int i = 1; i<n; i++){
+            if(intervals[i][0] > res.back()[1]){
                 res.push_back(intervals[i]);
             }else{
                 res.back()[1] = max(res.back()[1] , intervals[i][1]);
