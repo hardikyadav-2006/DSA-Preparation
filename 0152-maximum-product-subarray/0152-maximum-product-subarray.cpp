d0d7ddd9f571dc8c2b0571994e372b0da2723ae6
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-25 08:53:02 +0530

    Time: 0 ms (100%), Space: 17.8 MB (58.47%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,21 +4,21 @@ public:
         int n = nums.size();
         int pre = 1;
         int suff = 1;
-        int max_pdt = INT_MIN;
-        for(int i = 0;i<n;i++){
-            if(pre == 0){
+        int max_product = INT_MIN;
+        for(int i = 0;i<n; i++){
+            pre *= nums[i];
+            suff *= nums[n-i-1];
+
+            max_product = max(max_product, max(pre, suff));
+
+             if(pre == 0){
                 pre = 1;
             }
             if(suff == 0){
                 suff = 1;
             }
-
-            pre *= nums[i];
-            suff *= nums[n-i-1];
-            max_pdt = max(max_pdt, max(pre, suff)); 
-               
         }
-        return max_pdt;
+        return max_product;
         
     }
 };
