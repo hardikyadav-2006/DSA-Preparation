e4d1d0a2376c7d33f9f3e70678beaf372472dd05
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-26 10:39:29 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.53%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int n = nums.size();
         int st = 0;
         int end = n-1;
@@ -9,38 +9,37 @@ public:
             mid = (st+end)/2;
             if(nums[mid] == target){
                 return true;
-            }
-            else if(nums[mid] == nums[st] && nums[mid] == nums[end]){
+            }else if(nums[end] == nums[mid] && nums[mid] == nums[st]){
                 int sum1 = 0;
-                int sum2 = 0;
-                for(int i = st; i<=mid; i++){
+                int sum2= 0;
+                for(int i = st; i<= mid; i++){
                     sum1+= nums[i];
-                }for(int i = mid; i<=end; i++){
+                }
+                for(int i = mid; i<= end; i++){
                     sum2+= nums[i];
                 }
-                if(sum1!= (mid-st+1)*nums[mid]){
+                if(sum1 != (mid-st+1)*nums[mid]){
+                    end = mid-1;
+                }else{
+                    st= mid+1;
+                }
+            }
+            else if( nums[mid]> nums[end]){
+                if(target >= nums[st] && target < nums[mid]){
                     end = mid-1;
                 }else{
                     st = mid +1;
                 }
-                
-            }else if(nums[mid] >= nums[st]){
-                if(target < nums[mid] && target >= nums[st]){
-                    end = mid-1;
-                }else{
-                    st=mid+1;
-                }
-            }else if(nums[mid]<=nums[end]){
-
+            }else if(nums[mid] <= nums[end]){
                 if(target > nums[mid] && target <= nums[end]){
-                    st = mid+1;
+                    st = mid +1;
+                    
                 }else{
                     end = mid-1;
                 }
 
             }
         }
-        return false;
-        
+        return false;        
     }
 };
