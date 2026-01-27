4f17b33ab878165a84034f4839045a84aec315de
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-27 09:21:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (85.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,19 +5,20 @@ public:
         int st = 1;
         int end = n-2;
         int mid = (st+end)/2;
-        if(n==1){
-            return 0;
-        }
-        else if(nums[0]>nums[1]) return 0;
-        else if(nums[n-2]<nums[n-1]) return n-1;
+
+        if(n==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
         while(st<=end){
             mid = (st+end)/2;
-            if(nums[mid-1]<nums[mid] && nums[mid+1]<nums[mid]){
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]){
                 return mid;
-            }else if(nums[mid]<nums[mid+1]){
+            }else if(nums[mid] < nums[mid+1]){
                 st= mid+1;
             }else{
                 end = mid-1;
+                
             }
         }
         return -1;
