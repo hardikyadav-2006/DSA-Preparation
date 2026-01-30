530e712eb229238707835ce5ddebee3827609cc8
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-30 18:09:52 +0530

    Time: 64 ms (20.94%), Space: 18.7 MB (66.82%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,19 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-        int r = row-1;
-        int c = 0;
-        while(r>=0 && c<col){
+        int r = 0;
+        int c = col-1;
+        while(r<row && c>=0){
             int mid = matrix[r][c];
             if(mid == target){
                 return true;
-            }else if(mid < target){
-                c++;
+            }else if(mid > target){
+                c--;
             }else{
-                r--;
+                r++;
             }
         }
         return false;
-
+        
     }
 };
