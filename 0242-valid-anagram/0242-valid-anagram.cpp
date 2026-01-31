71510bb24217634258a2e78c42f8b420fb861b19
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-31 12:21:54 +0530

    Time: 8 ms (7.31%), Space: 9.9 MB (10.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,10 +6,29 @@ public:
 
         if(n != n1) return false;
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        unordered_map<char, int> m;
+        for(int i = 0; i<n; i++){
+            if(m.find(s[i]) != m.end()){
+                m[s[i]] +=1;
+            }else{
+                m.emplace(s[i], 1);
+            }
+        }
 
-        return (s == t);
+        for(int i = 0; i<n; i++){
+            if(m.find(t[i]) == m.end()){
+                return false;
+            }else{
+                m[t[i]] -= 1;
+            }
+        }
+
+        for(auto p : m){
+            if(p.second != 0) return false;
+        }
+        return true;
+
+
 
 
         
