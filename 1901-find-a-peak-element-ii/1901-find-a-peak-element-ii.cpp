73417741f5306f14c006419aefa45d1f652459fa
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-31 10:39:49 +0530

    Time: 2 ms (9.84%), Space: 50.5 MB (67.59%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -7,33 +7,29 @@ public:
 
         int st = 0;
         int end = col-1;
+        int max_row = -1;
+        int max_el = INT_MIN;
         while(st<=end){
             int mid = (st+end)/2;
-            int max_row = -1;
-            int tmp = INT_MIN;
             for(int i = 0; i<row; i++){
-                if (tmp<mat[i][mid]){
-                    tmp = mat[i][mid];
+                if(mat[i][mid] > max_el){
+                    max_el = mat[i][mid];
                     max_row = i;
                 }
             }
 
-            int left = (mid >0) ? mat[max_row][mid-1]: -1;
-            int right = (mid<col-1) ? mat[max_row][mid+1] : -1;
+            int left = (mid == 0) ? -1 : mat[max_row][mid-1];
+            int right = (mid == col-1) ? -1 : mat[max_row][mid+1];
 
-            if(mat[max_row][mid]>left && mat[max_row][mid] >right){
-                res.push_back(max_row);
-                res.push_back(mid);
-                return res;
-            }else if(mat[max_row][mid]<right){
+            if(mat[max_row][mid] > left && mat[max_row][mid]>right){
+                return {max_row, mid};
+            }else if(mat[max_row][mid] < right){
                 st = mid+1;
             }else{
                 end = mid-1;
             }
         }
         return {-1,-1};
-
-
         
         
     }
