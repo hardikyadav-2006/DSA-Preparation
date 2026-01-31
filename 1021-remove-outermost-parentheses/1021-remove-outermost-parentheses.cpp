7e67d3ed5abd2e81d276133e1ee81ec22bccfba9
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-01-31 10:20:43 +0530

    Time: 1 ms (25.62%), Space: 8.8 MB (80.71%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         int count = 0;
-        int n = s.size();
         string tmp = "";
+        int n = s.size();
         for(int i = 0; i<n; i++){
-            
             if(s[i] == ')'){
                 count --;
             }
-
-            if(count >0){
-                tmp += s[i];
+            if(count > 0){
+                tmp+= s[i];
             }
             if(s[i] == '('){
                 count ++;
             }
+            
         }
-        return tmp;
-
+        return  tmp;
     }
 };
