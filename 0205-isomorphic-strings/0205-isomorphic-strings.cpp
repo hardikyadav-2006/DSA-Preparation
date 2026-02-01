fa4f4e10fd3cbf530936b984b3d83bd6738f97f7
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-01 10:40:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,23 +3,19 @@ public:
     bool isIsomorphic(string s, string t) {
         int n = s.size();
         int n1 = t.size();
-        if(n!=n1) return false;
+        if(n != n1) return false;
 
-        unordered_map <char, char> m;
+        unordered_map<char, char> m;
         for(int i = 0; i<n; i++){
             if(m.find(s[i]) != m.end()){
-                if(t[i] != m[s[i]]){
+                if(m[s[i]] != t[i]){
                     return false;
                 }
             }else{
-                for(auto p : m){
-                    if(p.second == t[i]){
-                        return false;
-                    }
-                }
-                m.emplace(s[i], t[i]);
+                m.emplace(s[i],t[i]);
             }
         }
         return true;
+        
     }
 };
