b0dabaa842d9a9b3ce42d41ecc22fe8a8c347f3c
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-01 10:17:15 +0530

    Time: 4 ms (35.41%), Space: 10 MB (10.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,32 +3,20 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.size();
         int n1 = t.size();
-
-        if(n != n1) return false;
-
+        if(n!=n1) return false;
         unordered_map<char, int> m;
-        for(int i = 0; i<n; i++){
-            if(m.find(s[i]) != m.end()){
-                m[s[i]] +=1;
-            }else{
-                m.emplace(s[i], 1);
-            }
 
-            if(m.find(t[i]) == m.end()){
-                m.emplace(t[i], -1);
-            }else{
-                m[t[i]] -= 1;
-            }
+        for(int i = 0; i<n; i++){
+            m[s[i]]++;
+            m[t[i]]--;
         }
 
         for(auto p : m){
-            if(p.second != 0) return false;
+            if(p.second != 0){
+                return false;
+            }
         }
         return true;
-
-
-
-
         
     }
 };
