76cb52eeb7e2280097e2b9667e14dd82a419bc0c
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-05 00:06:37 +0530

    Time: 83 ms (36.11%), Space: 164.8 MB (17.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,35 +2,34 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        if(n <= 0) return s;
-        bool found = false;
+        if(n==0 || n==1) return s;
         int start = 0;
         int end = 0;
-        string tmp = "";
-        for(int i = 0; i<n; i++){
-            int odd = expand_arr(s, i, i);
-            int even = expand_arr(s,i,i+1);
-            int max_len = max(even, odd);
+        for(int i = 0;i<n; i++){
+            int odd = expand(s,i,i);
+            int even = expand(s,i,i+1);
+            int res = max(odd, even);
 
-            if(max_len > end - start){
-                start = i-(max_len-1)/2;
-                end = i +  max_len/2;
+            if(res>end-start){
+                start = i-((res-1)/2);
+                end = i+(res/2);
+
             }
-
             
-        }
-        return s.substr(start, end - start+1);
-    }
-        
 
-    int expand_arr(string s, int left, int right){
+
+        }
+        return s.substr(start, end-start+1);
+        
+    }
+
+    int expand(string s, int left, int right){
         int n = s.size();
-        while(left>=0 && right<n && s[left] == s[right]){
+        while(left>=0 &&right<n && s[left] == s[right]){
             left--;
             right++;
         }
         return right-left-1;
-    }
         
-    
+    }
 };
