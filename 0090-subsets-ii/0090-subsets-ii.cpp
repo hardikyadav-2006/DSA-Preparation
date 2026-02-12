8fbb9d2639dc44c167f68ae3887c7f890f677810
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-12 23:00:31 +0530

    Time: 0 ms (100%), Space: 10.4 MB (67.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,37 +1,29 @@
-
 class Solution {
 public:
-    void helper(vector<int>& nums, int i, vector<vector<int>>& res,vector<int> tmp){
-        
-        
-        int n = nums.size();
-        if(i  == n){
-            res.push_back(tmp);
+
+    void allSubsets(vector<int>& nums, vector<int> &ans, int i, vector<vector<int>> &res){
+        if(i == nums.size()){
+            res.push_back({ans});
             return;
         }
-        tmp.push_back(nums[i]);
-        helper(nums, i+1,res,tmp);
-        tmp.pop_back();
-        int idx = i+1;
-        while(idx<n && nums[idx] == nums[idx-1]){
-            idx++;
-        }
-        
-        helper(nums, idx,res,tmp);
 
+        ans.push_back(nums[i]);
+        allSubsets(nums, ans, i+1, res);
+
+        ans.pop_back();
+        int idx = i+1; // idx initiated with i+1 because below if we remove idx we need to pass i+1
+        while(idx < nums.size() && nums[idx] == nums[idx-1]) idx++;
+        allSubsets(nums, ans, idx, res);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> res;
-        vector<int> tmp;
-        helper(nums, 0, res, tmp);
-        
+        vector<int> ans;
+        int i = 0;
+        allSubsets(nums, ans, i, res);
         return res;
-        
-
-
-        
-        
     }
+        
+    
 };
