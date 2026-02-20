9886d1a01305491ead63861c1d0472b3669ea288
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-20 21:22:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void helper(double x, long long int n, double &res){
-        
-        if(n == 1){
+    void helper(double x,long long int n, double& res){
+        if(n==1){
             res = res*x;
             return;
-
-        } 
-        if(n%2 == 0){
-            helper(x*x, n/2, res);
-        }else{
-            res = res*x;
-            helper(x*x, n/2, res);
         }
-    }
+        if(n%2==0){
+            x=x*x;
+            helper(x, n/2,res);
+        }else{
+            res=res*x;
+            x=x*x;
+            helper(x, n/2, res);
+        }
+
+    } 
     double myPow(double x, long long int n) {
-        if(n== 0 ) return 1;
-        double res = 1;
+        if(n==0) return 1;
         if(n<0){
             n=-n;
             x=1/x;
         }
+        double res = 1;
         helper(x, n, res);
         return res;
-
-
         
     }
 };
