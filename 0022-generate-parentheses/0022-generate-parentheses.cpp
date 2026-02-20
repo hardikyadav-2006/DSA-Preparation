c9fc1b59d7fbbc83cd0c1905983fa4d252fd4c53
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-20 21:28:48 +0530

    Time: 0 ms (100%), Space: 12.8 MB (96.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void helper(int n, vector<string> & res, string& tmp, int x, int y){
-        if(x==n&& y==n){
+
+    void helper(int n, vector<string>& res, int x, int y, string& tmp){
+        if(x==n&& y==n) {
             res.push_back(tmp);
-            return;
+            return ;
         }
         if(x<n){
-            tmp +='(';
-            helper(n, res, tmp, x+1, y);
-            tmp.pop_back();
-        }if(y<x){
-            tmp +=')';
-            helper(n, res, tmp, x, y+1);
+            tmp+='(';
+            helper(n,res,x+1,y,tmp);
             tmp.pop_back();
 
         }
-
+        
+        if(y<x){
+            tmp+=')';
+            helper(n,res,x,y+1,tmp);
+            tmp.pop_back();
+        }
     }
+
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         string tmp = "";
-        int x = 0;
-        int y = 0;
-        helper(n, res, tmp, x, y);
-        
-        return res;
-        
+        helper(n, res, 0, 0, tmp);
+        return res;  
     }
 };
