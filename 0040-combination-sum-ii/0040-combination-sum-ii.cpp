035b59ad0effd70f0bcda93402805434e16216aa
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-24 17:49:29 +0530

    Time: 0 ms (100%), Space: 13.9 MB (98.25%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,33 +1,29 @@
-
 class Solution {
 public:
-
-    void helper(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& tmp, int i){
+    void helper(vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& tmp, int i ){
         int n = candidates.size();
-        if(target==0){
-            ans.push_back(tmp);
-            return;
-        } 
-        if(i == n || target<0){
+        if(target == 0){
+            res.push_back(tmp);
             return;
         }
-            
+        if(target<0 || i>=n) return;
         tmp.push_back(candidates[i]);
-        helper(candidates, target-candidates[i], ans, tmp, i+1);
+        helper(candidates, target-candidates[i], res, tmp, i+1);
         tmp.pop_back();
-        int next_idx = i+1;
-        while(next_idx<n && candidates[next_idx] == candidates[i] ) {
-            next_idx++;
+        int nxt_idx = i+1;
+        while( nxt_idx <n &&candidates[i] == candidates[nxt_idx] ){
+            nxt_idx++;
         }
-        helper(candidates, target, ans, tmp, next_idx);
+        helper(candidates, target, res, tmp, nxt_idx);
 
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> res;
         vector<int> tmp;
         int i =0;
-        sort(candidates.begin(), candidates.end());
-        helper(candidates, target, ans, tmp, i);
-        return ans;
+        helper(candidates, target, res, tmp, i);
+        return res;
+        
     }
 };
