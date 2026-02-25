2731239d0de1799a92c746ed4ee66cd2f98ed03f
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-25 19:12:53 +0530

    Time: 0 ms (100%), Space: 9.9 MB (7.85%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    void helper(string digits, vector<string>& res, string& tmp, int i){
-        vector<string> imp={"abc", "def", "ghi","jkl","mno","pqrs", "tuv", "wxyz"};
+    void helper(string digits, vector<string>& ans, string& tmp, int i){
         int n = digits.size();
+        vector<string> link = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         if(i==n){
-            res.push_back(tmp);
+            ans.push_back(tmp);
             return;
         }
-        string asd = imp[digits[i]-50];
-        for(char ch : asd){
-            tmp.push_back(ch);
-            helper(digits, res, tmp, i+1);
+        string asd = link[digits[i]-50];
+        for(int j = 0; j<asd.size(); j++){
+            tmp.push_back(asd[j]);
+            helper(digits, ans, tmp, i+1);
             tmp.pop_back();
         }
-
     }
     vector<string> letterCombinations(string digits) {
-        
-        vector<string> res;
-        string tmp ="";
+        vector<string> ans;
+        string tmp = "";
         int i = 0;
-        int j = 0;
-        int k =0;
-        helper(digits, res, tmp, i);
-        return res;
-
+        helper(digits, ans, tmp, i);
+        return ans;
         
     }
 };
