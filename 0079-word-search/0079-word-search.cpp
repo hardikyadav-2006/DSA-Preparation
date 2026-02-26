af045b2a75fae64d6f48591c7c93288d9315c93a
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-02-26 19:16:11 +0530

    Time: 139 ms (96.07%), Space: 10.6 MB (69.22%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    bool helper(vector<vector<char>>& board, string& word, int i, int j, int k){
-        int row = board.size();
-        int col = board[0].size();
-        int n = word.size();
-        if(k==n-1 ){
+    bool helper(vector<vector<char>>& board, string& word, int row, int col, int i){
+        int p = word.size();
+        int m = board.size();
+        int n = board[0].size();
+        if(i==p){
             return true;
         }
-        board[i][j] = '.';
-        if( j-1>=0 && board[i][j-1] == word[k+1]){            
-            if (helper(board, word, i, j-1, k+1)) return true;            
+        board[row][col] = '.';
+        if(col-1>=0 && board[row][col-1] == word[i]){
+            if(helper(board, word, row, col-1, i+1)) return true;
         }
-        if(i-1>=0 && board[i-1][j] == word[k+1]){            
-            if (helper(board, word, i-1, j, k+1)) return true;            
+        if(row-1>=0 && board[row-1][col] == word[i]){
+            if(helper(board, word, row-1, col, i+1)) return true;
         }
-        if(j+1<col && board[i][j+1] == word[k+1]){            
-            if (helper(board, word, i, j+1, k+1)) return true;            
+        if(col+1<n && board[row][col+1] == word[i]){
+            if(helper(board, word, row, col+1, i+1)) return true;
         }
-        if(i+1<row && board[i+1][j] == word[k+1]){            
-            if (helper(board, word, i+ 1, j, k+1)) return true;            
+        if(row+1<m && board[row+1][col] == word[i]){
+            if(helper(board, word, row+1, col, i+1)) return true;
         }
-        board[i][j] = word[k];
+        board[row][col] = word[i-1];
+        return false;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(board[i][j]==word[0]){
+                    if(helper(board, word, i, j, 1)) return true;
+
+                }
+                
+            }
+        }
         return false;
         
     }
-    bool exist(vector<vector<char>>& board, string word) {
-        int row = board.size();
-        int col = board[0].size();
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(board[i][j] == word[0]){
-                    if(helper(board, word, i, j, 0)) return true;
-                }
-            }
-        }
-        return false; 
-    }
+
 };
