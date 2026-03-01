02cb932a03199c7ae98cd869207dc7d3242ade3c
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-03-01 18:09:47 +0530

    Time: 36 ms (68.11%), Space: 54.4 MB (68.1%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    bool isPal(string s, int idx, int i){
-        string tmp1 = s.substr(idx, i+1-idx);
-        string tmp2 = s.substr(idx, i+1-idx);
-        reverse(tmp2.begin(), tmp2.end());
-        if(tmp1 == tmp2) return true;
-        return false;
-        
+    bool isPal(string s){
+        string temp = s;
+        reverse(s.begin(),s.end());
+        return temp == s;
     }
-
-    void helper(string s, vector<vector<string>>& res, vector<string>& tmp, int idx){
-        int n = s.size();
-        if(idx == n){
-            res.push_back(tmp);
-            return ;
-
+    void getAllParts(string s, vector<vector<string>> &ans, vector<string> &partition ){
+        if(s.empty()){
+            ans.push_back(partition);
+            return;
         }
-        for(int i = idx; i<n; i++){
-            if(isPal(s, idx, i)){
-                tmp.push_back(s.substr(idx,i+1-idx));
-                helper(s, res, tmp, i+1);
-                tmp.pop_back();
+
+        for(int i = 0; i<s.size(); i++){
+            if(isPal(s.substr(0,i+1))){
+                partition.push_back(s.substr(0,i+1));
+                getAllParts(s.substr(i+1), ans, partition);
+                partition.pop_back();
             }
         }
-        
     }
+
     vector<vector<string>> partition(string s) {
-        vector<string> tmp;
-        vector<vector<string>> res;
-        int i = 0;
-        helper(s, res, tmp, i);
-        return res;  
+        vector<vector<string>> ans;
+        vector<string> partition;
+    
+        getAllParts(s, ans, partition);
+        return ans;
     }
 };
