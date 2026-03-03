f1427af3f0e4efd3c774ea277a0e719c9ce856b7
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-03-03 14:51:37 +0530

    Time: 41 ms (62.24%), Space: 54.7 MB (63.29%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     bool isPal(string s){
-        string tmp = s
+        string tmp = s;
         reverse(tmp.begin(), tmp.end());
-        return tmp == s;
+        return s == tmp;
     }
-
-    void helper(string s, vector<vector<string>>& res, vector<string>&tmp){
-        int n = s.size();
+    void helper(string s, vector<vector<string>>& res, vector<string>& tmp){
         if(s.empty()){
             res.push_back(tmp);
             return;
         }
-        for(int i=0; i<n;i++){
+        int n = s.size();
+        for(int i = 0; i<n; i++){
             if(isPal(s.substr(0,i+1))){
                 tmp.push_back(s.substr(0,i+1));
                 helper(s.substr(i+1), res, tmp);
                 tmp.pop_back();
-            }
-           
 
+            }
+            
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> tmp;
         helper(s, res, tmp);
-        return res; 
+        return res;
     }
 };
