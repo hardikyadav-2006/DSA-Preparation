2c5cf1d85d0df905d0e420a356d1815a4476d993
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-03-09 21:46:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -7,23 +7,23 @@ public:
 
         int n2 = nums2.size();
         int n1 = nums1.size();
-        for (int i = 0; i<n2 ; i++){
-            while(!s.empty() && nums2[i]>s.top()){
-                m.emplace(s.top(), nums2[i]);
-                s.pop();
-            }
+        for (int i = n2-1; i>=0 ; i--){
+            while(s.size()>0 && s.top()<=nums2[i]) s.pop();
+            if(s.empty()) m.emplace(nums2[i],-1);
+            else m.emplace(nums2[i], s.top());
             s.push(nums2[i]);
         }
         
-        while(!s.empty()){
-            m.emplace(s.top(), -1);
-            s.pop();
-        }
+        
 
         for(int i = 0; i<n1;i++){
+            
             res.push_back(m[nums1[i]]);
         }
         return res;
+
+
+
         
     }
 };
