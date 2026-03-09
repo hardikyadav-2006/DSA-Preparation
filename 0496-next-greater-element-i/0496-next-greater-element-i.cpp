a156f8491cb2852a86aa57c6eac47a0d15647ba7
Hardik Yadav <hardikyadavparasf9@gmail.com>
2026-03-09 21:34:32 +0530

    Time: 4 ms (24.08%), Space: 12.7 MB (73.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,23 +4,17 @@ public:
         vector<int> res;
         unordered_map<int, int> m;
         stack<int> s;
-        int n = nums2.size();
+
+        int n2 = nums2.size();
         int n1 = nums1.size();
-        for (int i = 0; i<n ; i++){
-            if(s.empty()) s.push(nums2[i]);
-            else{
-                if(nums2[i] > s.top()){
-                    while(!s.empty() && nums2[i]>s.top()){
-                        m.emplace(s.top(), nums2[i]);
-                        s.pop();
-                    }
-                    s.push(nums2[i]);
-                }
-                else{
-                    s.push(nums2[i]);
-                }
+        for (int i = 0; i<n2 ; i++){
+            while(!s.empty() && nums2[i]>s.top()){
+                m.emplace(s.top(), nums2[i]);
+                s.pop();
             }
+            s.push(nums2[i]);
         }
+        
         while(!s.empty()){
             m.emplace(s.top(), -1);
             s.pop();
@@ -30,9 +24,6 @@ public:
             res.push_back(m[nums1[i]]);
         }
         return res;
-
-
-
         
     }
 };
